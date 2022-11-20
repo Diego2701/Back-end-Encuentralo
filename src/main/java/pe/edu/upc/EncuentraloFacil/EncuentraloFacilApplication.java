3c7d2e133f0e14ea88c180a9c592583ddb64370e
Diego <diegoporras270@gmail.com>
2022-11-20 10:52:42 -0500

    Heroku deploy1

--- src/main/java/pe/edu/upc/EncuentraloFacil/EncuentraloFacilApplication.java
+++ src/main/java/pe/edu/upc/EncuentraloFacil/EncuentraloFacilApplication.java
@@ -2,12 +2,24 @@ package pe.edu.upc.EncuentraloFacil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EncuentraloFacilApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EncuentraloFacilApplication.class, args);
+	}
+	@Bean
+	WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET","POST","PUT","DELETE");
+			}
+		};
 	}
 
 }
